add0ee83a7727defa09071c92686be3ff23985c6
CLEVER <clevereu2023@gmail.com>
2026-02-03 10:44:16 +0100

    First commit

---
+++ src/es/studium/Funciones.java
@@ -0,0 +1,16 @@
+package es.studium;
+
+import java.awt.TextField;
+
+public class Funciones
+{
+	Funciones()
+	{
+	} // Constructor
+
+	public float calcularIva(TextField txt1, TextField txt2)
+	{
+			float resultado = Float.parseFloat(txt1.getText()) * Float.parseFloat(txt2.getText()) / (float) 100.0;
+			return resultado;	
+	}
+}
